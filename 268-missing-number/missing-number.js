de36bc61f5961f6b976e5e466a61144b88bdf4b4
harshsparmar <93429115+harshsparmar@users.noreply.github.com>
2024-08-13 14:15:35 +0530

    Time: 54 ms (79.06%) | Memory: 51.1 MB (71.47%) - LeetSync

---
+++ 268-missing-number/missing-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
+    let sum = 0;
+    let othersum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i]
+        othersum += (nums.length - i)
+    }
+    return othersum - sum
+};
